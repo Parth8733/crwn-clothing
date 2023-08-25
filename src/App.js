34b60695f1868112cf8cfc479a54815b294fbd78
Parth Patel <parth8733@gmail.com>
2023-08-25 02:48:50 -0400

    Replace Sign In route with Authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component.jsx";
 import Home from "./routes/home/home.component";
-import SignIn from "./routes/sign-in/sign-in.component.jsx";
+import Authentication from "./routes/authentication/authentication.component.jsx";
 
 const Shop = () => {
   return <h1>I am Shop Pge</h1>;
@@ -12,7 +12,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
