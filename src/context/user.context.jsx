1f01ac044254b3f0deb952cd33de2345850067e9
Parth Patel <parth8733@gmail.com>
2023-08-28 19:26:33 -0400

    Implemented OnAuthStateChanged Listener

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { onAuthStateChanedListener } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
   currentUser: null,
@@ -6,6 +7,13 @@ export const UserContext = createContext({
 });
 
 export const UserProvider = ({ children }) => {
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanedListener((user) => {
+      console.log(user);
+    });
+    return unsubscribe;
+  }, []);
+
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  onAuthStateChanged,
 } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
@@ -70,3 +71,6 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 };
 
 export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChanedListener = (callBack) =>
+  onAuthStateChanged(auth, callBack);
