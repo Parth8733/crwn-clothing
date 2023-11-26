7f6c35ca1ab76fcfec1f81ddf87029bfbd545e80
Parth Patel <parth8733@gmail.com>
2023-11-26 13:33:04 -0500

    Demystifying Middleware

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,23 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const customLogger = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type:", action.type);
+  console.log("payload:", action.payload);
+  console.log("currentState:", store.getState());
+
+  next(action);
+
+  console.log("nextState:", store.getState());
+};
+
+// const middleWares = [logger];
+const middleWares = [customLogger];
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(rootReducer, undefined, composedEnhancers);
