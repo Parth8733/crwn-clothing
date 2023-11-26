54d4c16d22542c66926a6bd6b7762155b24bfd0b
Parth Patel <parth8733@gmail.com>
2023-11-26 18:57:45 -0500

    Implemented Redux-thunk for fetching Categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,18 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { useEffect } from "react";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/categories.action";
+// import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+// import { setCategories } from "../../store/categories/categories.action";
 import { useDispatch } from "react-redux";
-
+import { fetchCategoriesAsync } from "../../store/categories/categories.action";
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    };
-    getCategoriesMap();
+    // const getCategoriesMap = async () => {
+    //   const categoriesArray = await getCategoriesAndDocuments();
+    //   dispatch(setCategories(categoriesArray));
+    // };
+    // getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, []);
   return (
     <Routes>
--- src/store/store.js
+++ src/store/store.js
@@ -5,18 +5,20 @@ import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 import { persistReducer } from "redux-persist";
 import persistStore from "redux-persist/es/persistStore";
+import thunk from "redux-thunk";
 
 const persistConfig = {
   key: "root",
   storage,
-  blackList: ["user"],
+  blacklist: ["user"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 // const middleWares = [customLogger];
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
